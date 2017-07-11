d2480c081bfcf23020b72f449150380af5ec5f6b
Kristoffer Andersen <ka@ptype.dk>
2017-07-11 16:34:23 +0200

    Again more updates to use new 1.7 classes

--- main_view_controller.h
+++ main_view_controller.h
@@ -3,42 +3,40 @@
 #define main_view_controller_h
 
 #include <mono.h>
-#include "scene.h"
-#include "icon_view.h"
 
 using namespace mono;
 using namespace mono::ui;
 using namespace mono::geo;
 
 class IconRepeatVert : public View {
-    
+
     const MonoIcon *icon;
-    
+
 public:
-    
+
     Color front, back;
-    
+
     IconRepeatVert(const Rect &rct, const MonoIcon &icon);
-    
+
     void repaint();
-    
+
 };
 
-class MainViewController : public Scene {
+class MainViewController : public SceneController {
 protected:
-    
+
     bool celcius;
     int oldTemp;
     TextLabelView tempLbl;
     IconView bubleView, tempballView;
     IconRepeatVert warmThermView, coldThermView;
-    
+
 public:
-    
+
     MainViewController();
-    
+
     void setTemp(int t);
-    
+
 };
 
 #endif /* main_view_controller_h */
